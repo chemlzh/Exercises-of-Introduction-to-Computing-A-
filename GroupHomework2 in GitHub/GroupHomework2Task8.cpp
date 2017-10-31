df1c2df050c8122c8784f72ad3d2853b97969a30
chemlzh <chemlzh@pku.edu.cn>
2017-10-31 17:03:40 +0800

    Modify some programs.

--- GroupHomework2 in GitHub/GroupHomework2Task8.cpp
+++ GroupHomework2 in GitHub/GroupHomework2Task8.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
-long y, m, d = 1L;
+long y, m, d;
 long month[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 int main() {
 	scanf("%ld%ld", &y, &m);
 	printf("%s\n", " Sun Mon Tue Wed Thu Fri Sat");
 	if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) month[2]++;
-	for (long i = 1901; i < y; i++) {
+	for (long i = 1900; i < y; i++) {
 		if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) d = (d + 2) % 7; else d = (d + 1) % 7;
 	}
 	for (long i = 1; i < m; i++) d = (d + month[i]) % 7;
